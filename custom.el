1d501418d97659c3a44fff5ae6b0f91921891bde
Marcin <marcin@waltharius.pl>
2025-10-08 18:25:15 +0200

    dodanie pakietu yasnippet

--- custom.el
+++ custom.el
@@ -3,7 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(all-the-icons ample-theme consult-denote dashboard diff-hl
+		   doom-themes flyspell-correct-ivy gnuplot
+		   gruvbox-theme htmlize langtool magit marginalia
+		   orderless org-appear org-contrib org-roam-ql
+		   org-roam-ui org-transclusion solarized-theme
+		   undo-tree vertico wc-mode yasnippet
+		   yasnippet-snippets zenburn-theme))
  '(recentf-filename-handlers '(abbreviate-file-name)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
