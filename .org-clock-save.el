78d17abcdc79b19ddb79161ae32839858a2146a7
Marcin <marcin@waltharius.pl>
2025-12-11 22:36:48 +0100

    Auto-commit config: 2025-12-11 22:36
    Changed:
    .org-clock-save.el
    03-ui.el

--- .org-clock-save.el
+++ .org-clock-save.el
@@ -1,1 +1,1 @@
-;; .org-clock-save.el - azazel.home.lan at <2025-12-11 czw 22:32>  -*- lexical-binding: t; -*-
+;; .org-clock-save.el - azazel.home.lan at <2025-12-11 czw 22:34>  -*- lexical-binding: t; -*-
--- modules/03-ui.el
+++ modules/03-ui.el
@@ -141,10 +141,12 @@
 
 ;; Zmiana wyglądu bloków cytatów w Org-mode
 (custom-set-faces
- '(org-quote ((t (:background "#f9f9f9" :slant italic :foreground "#555555"))))
- '(org-block ((t (:background "#fef8e0" :extend t :family "Georgia"))))
- '(org-block-begin-line ((t (:background "#e0e0e0" :foreground "#999999" :height 0.9))))
- '(org-block-end-line ((t (:background "#e0e0e0" :foreground "#999999" :height 0.9)))))
+ '(org-block-begin-line ((t (:background "#e8e5d7" :foreground "#8b7355"
+                             :slant italic :underline t))))
+ '(org-block ((t (:background "#faf8f0" :foreground "#4a4a4a"
+                  :slant italic :extend t))))
+ '(org-block-end-line ((t (:background "#e8e5d7" :foreground "#8b7355"
+                           :slant italic :overline t)))))
 (setq org-fontify-quote-and-verse-blocks t)
 
 ;; Zamień znaczniki bloków na symbole Unicode
