b5ec7a822e51c9235109b23fb80d57cfe8b86a6f
marcin <nixosgitemail.frivolous320@passmail.net>
2026-07-26 13:08:08 +0200

    Auto-commit config: 2026-07-26 13:08
    Changed:
    custom.el
    function_helper.org

--- custom.el
+++ custom.el
@@ -7,16 +7,7 @@
  '(custom-safe-themes
    '("138ed99a323c1b93c52f4b3726caf2bc634b79a76fa63a3d3aff76394db5f28f"
      default))
- '(package-selected-packages
-   '(all-the-icons citar-denote consult-denote dashboard denote-explore
-                   dired-sidebar flycheck-package flyspell-correct-ivy
-                   gnuplot highlight-parentheses htmlize keyfreq
-                   langtool magit marginalia modus-themes nov
-                   orderless org-appear org-bullets org-journal
-                   org-kanban org-noter-pdftools org-roam-ui
-                   org-super-agenda org-superstar org-transclusion
-                   ox-hugo rainbow-delimiters undo-tree vertico
-                   writeroom-mode yasnippet-snippets))
+ '(package-selected-packages nil)
  '(recentf-filename-handlers '(abbreviate-file-name))
  '(safe-local-variable-values
    '((eval progn (visual-fill-column-mode -1)
