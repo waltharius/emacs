1430f3a13e68882233cd4ff1e9c3fbfc398e3b9f
marcin <nixosgitemail.frivolous320@passmail.net>
2026-08-13 23:34:26 +0200

    some modification for custom.el done from GUI level

--- custom.el
+++ custom.el
@@ -7,6 +7,9 @@
  '(custom-safe-themes
    '("138ed99a323c1b93c52f4b3726caf2bc634b79a76fa63a3d3aff76394db5f28f"
      default))
+ '(display-battery-mode t)
+ '(display-line-numbers-type 'relative)
+ '(display-time-mode t)
  '(package-selected-packages nil)
  '(recentf-filename-handlers '(abbreviate-file-name))
  '(safe-local-variable-values
